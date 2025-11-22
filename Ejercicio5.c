b4eb15d24e315d0daa48495447f8def7856a62be
naomiacontreras <naomi.miranda.contreras@gmail.com>
2025-11-21 20:05:11 -0600

    Create Ejercicio5.c

---
+++ Ejercicio5.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+ FILE *archivo;
+ char escribir[] = "Escribir cadena en archivo mediante fprinft. \nFacultad de 
+Ingeniería.\n";
+ archivo = fopen("fprintf.txt", "r+");
+ if (archivo != NULL)
+ {
+ fprintf(archivo, escribir);
+ fprintf(archivo, "%s", "UNAM\n");
+ fclose(archivo);
+ }
+ else
+ {
+ printf("El archivo no existe o no se tiene permisos de lectura/escritura.\n");
+ }
+ return 0;
+}
